a930bdc56bdce3bfce78cf77d268a79490bdc2bf
chinara <chinara.isabaeva@gmail.com>
2015-11-24 17:10:17 +0000

    add webbhook get handler

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello chee-bot'
+
+@app.route('/getUpdates/<token>')
+def get_updates():
+    return Response(status=200)
+
 
 if __name__ == '__main__':
     app.run()
