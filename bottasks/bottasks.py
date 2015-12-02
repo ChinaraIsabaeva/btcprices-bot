6a2630bf77beea4286c6a6e57236811fac9f0c62
chinara <chinara.isabaeva@gmail.com>
2015-12-02 12:37:37 +0000

    change port and some other staff

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -2,21 +2,23 @@ import os, json
 
 from flask import Flask, request
 
+
+WEBHOOK_URL_PATH = "/getUpdates/%s/" % (os.environ['TOKEN'])
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello chee-bot'
 
-@app.route('/getUpdates/', methods=['POST'])
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def get_updates():
-    try:
-        data = request.get_data()
-        return data
-    except Exception:
-        print Exception.message
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data()
+        return ''
+    else:
+        return 'something was wrong'
         
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port='8080')
+    app.run(debug=True, port='8443')
