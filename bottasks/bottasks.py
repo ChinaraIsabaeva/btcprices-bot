97ffffde1b44b5415ccb1053742d3116f6f92c8a
chinara <chinara.isabaeva@gmail.com>
2015-12-02 14:14:18 +0000

    make post empty

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -14,7 +14,7 @@ def home():
 @app.route(WEBHOOK_URL_PATH, methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
-        return request
+        return request.data
     else:
         return 'It was get request'
         
