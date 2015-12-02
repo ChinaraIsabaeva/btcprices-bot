03ae38a181c817e237787eefc8d184fc456c044b
chinara <chinara.isabaeva@gmail.com>
2015-12-02 14:38:04 +0000

    check logs

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -3,7 +3,7 @@ import os, json
 from flask import Flask, request, redirect, url_for
 
 WEBHOOK_URL_PATH = "/getUpdates/%s/" % (os.environ['TOKEN'])
-PORT = os.environ['PORT']
+PORT = int(os.environ['PORT'])
 
 app = Flask(__name__)
 
