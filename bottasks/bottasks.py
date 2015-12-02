b01913d4a23ddd49a1be6355c8f1c25dd3e9fc3c
chinara <chinara.isabaeva@gmail.com>
2015-12-02 14:42:00 +0000

    print request headers

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -19,7 +19,7 @@ def last_updated(data):
 @app.route(WEBHOOK_URL_PATH, methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
-        print request.data
+        print request.headers
         return 'OK'
 
 
