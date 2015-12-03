7acfaa56bc1d9b004726d27bcc95e5c292b89622
chinara <chinara.isabaeva@gmail.com>
2015-12-03 14:08:47 +0000

    add some other msgs

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -24,12 +24,14 @@ def last_updated(data):
 def webhook():
     if request.method == 'POST':
         updates = request.get_json()
-        print (updates['message']['text'] == 'price')
+        chat_id = updates['message']['chat']['id']
+        text = ''
+        message = {'chat_id': chat_id, 'text': text}
         if updates['message']['text'] == 'price':
-            chat_id = updates['message']['chat']['id']
-            message_id = updates['message']['message_id']
-            message = {'chat_id': chat_id, 'text': 'code succed'}
-            requests.post("https://api.telegram.org/bot120560818:AAHKRbbHYEM9l7PIxuW1-3alAGQ1PV0NeUE/sendMessage", json=message)
+            text = 'You can get BTC prices soon'
+        if updates['message']['text'] in ['привет', 'Привет', 'hi', 'Hi', 'HI', 'hello', 'Hello']:
+            text = 'Привет'
+        requests.post("https://api.telegram.org/bot120560818:AAHKRbbHYEM9l7PIxuW1-3alAGQ1PV0NeUE/sendMessage", json=message)
         return 'OK'
 
     
