5aa5af97d4bae12fdab8cfc8f16b0692aa69c7db
chinara <chinara.isabaeva@gmail.com>
2015-12-03 13:15:02 +0000

    another try

--- bottasks/bottasks.py
+++ bottasks/bottasks.py
@@ -25,11 +25,9 @@ def webhook():
     if request.method == 'POST':
         updates = request.get_json()
         if updates['message']['text'] == 'price':
-            print type(updates['message']['chat']['id'])
-            print type(updates['messae']['message_id'])
-            chat_id = int(updates['message']['chat']['id'])
-            message_id = int(updates['messae']['message_id'])
-            message = {"chat_id": chat_id, "text": "test", "reply_to_message_id": message_id}
+            chat_id = updates['message']['chat']['id']
+            message_id = updates['messae']['message_id']
+            message = {"chat_id": chat_id, "text": "code succed", "reply_to_message_id": message_id}
             requests.post('https://api.telegram.org/bot120560818:AAHKRbbHYEM9l7PIxuW1-3alAGQ1PV0NeUE/sendMessage', data=json.dumps(message))
         print "Use okay"
 
