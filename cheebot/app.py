3c60724a8b3e945f6539b12fd58a50393390b13a
chinara <chinara.isabaeva@gmail.com>
2015-12-14 14:47:04 +0000

    print updates

--- cheebot/app.py
+++ cheebot/app.py
@@ -23,16 +23,14 @@ def webhook():
         chat_id = updates['message']['chat']['id']
         text = ''
         message = {'chat_id': chat_id, 'text': text}
+        print (updates)
         if updates['message']['text'] == 'price':
-            print ('price')
             message['text'] = 'You could receive prices soon'
         elif updates['message']['text'] in ['привет', 'Привет', 'hi', 'Hi', 'HI', 'hello', 'Hello']:
             print ('hello')
             message['text'] = 'Привет'
         else:
-            print ('other')
             message['text'] = 'Я не знаю, что на это сказать'
-        print (message)
         requests.post('https://api.telegram.org/bot120560818:AAHKRbbHYEM9l7PIxuW1-3alAGQ1PV0NeUE/sendMessage', json=message)
         return 'OK'
 
