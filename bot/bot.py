c7076286f34fa36809b951f5e51f6d21272201a7
chinara <chinara.isabaeva@gmail.com>
2015-12-14 13:43:00 +0000

    add newline into msg

---
+++ bot/bot.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+TOKEN = os.environ['TOKEN']
+BASE_URL = "https://api.telegram.org/bot{token}/".format(token=TOKEN)
+
+
+class Bot(object):
+    def __init__(self, token):
+        self.token = token
+    
+    def _post_method(self. method=None, data=None):
+        response = requests.post(BASE_URL+method, json=data)
+        return response.status_code
+
+
+    def send_message(self, data):
+        response = _post_method(method='sendMessage', data=data)
+        return response.status_code
--- cheebot/app.py
+++ cheebot/app.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import os
-import json
-import requests
 
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
 
-WEBHOOK_URL_PATH = "/getUpdates/%s/" % (os.environ['TOKEN'])
+from bot.bot import Bot
+
+WEBHOOK_URL_PATH = "/getUpdates/{token}/".format(token=os.environ['TOKEN'])
 PORT = int(os.environ['PORT'])
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print ("Syasha ne prav")
     return 'Hello chee-bot'
-
-@app.route('/updates/')
-def last_updated(data):
-    return data
 
 @app.route(WEBHOOK_URL_PATH, methods=['POST'])
 def webhook():
@@ -28,7 +23,7 @@ def webhook():
         text = ''
         message = {'chat_id': chat_id, 'text': text}
         if updates['message']['text'] == 'price':
-            message['text'] = 'Скоро я смогу отправлять цену биткоина в долларах'
+            message['text'] = 'Hello \n to you'
         elif updates['message']['text'] in ['привет', 'Привет', 'hi', 'Hi', 'HI', 'hello', 'Hello']:
              message['text'] = 'Привет'
         else:
