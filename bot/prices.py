a22624fac4c1db0eb5d1cf999395affa4573d6cb
chinara <chinara.isabaeva@gmail.com>
2016-03-06 07:45:39 +0000

    edit code

--- bot/prices.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def get_prices():
-    message = requests.get('http://btcprices.info/api/latestprice').json()[0]
-    data = ''
-    for key in message:
-        data += key + ': ' + message[key] + '\n'
-    return data
