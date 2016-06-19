6835a3ce659c67274fc8b90ca72148758da369c0
chinara <chinara.isabaeva@gmail.com>
2016-06-19 17:32:20 +0100

    update message text

--- lib/prices.py
+++ lib/prices.py
@@ -3,7 +3,7 @@ import requests
 
 def get_prices():
     message = requests.get('http://btcprices.info/api/latestprice').json()[0]
-    data = ''
+    data = 'BTC prices on ' + message.pop('date') + ':\n'
     for key in message:
         data += key + ': ' + message[key] + '\n'
     return data
